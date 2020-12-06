95fb54f5f6a9a73a1d5e207868210c673440a841
Mohammad mahdi <mohammadmahdi@ubuntu.ubuntu-domain>
2020-12-07 03:24:06 +0330

    create <Idea> model and add it to model attribute of IdeaListView. now thats working.

--- blog/views.py
+++ blog/views.py
@@ -4,9 +4,11 @@ from django.views.generic import(
     ListView,
     DetailView,
 )
+from .models import Idea
 
 
 
 class IdeaListView(ListView):
+    model = Idea
     template_name = 'blog/home.html'
 
