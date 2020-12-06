b5dfa3a220416808c70cbf3fb22993c8fe16de52
Mohammad mahdi <mohammadmahdi@ubuntu.ubuntu-domain>
2020-12-07 03:10:29 +0330

    adding url for home page and create templates and  static folder. create base.html and home.html. but this still not working. we should create model and pass it in generic view.

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 
+from . import views
+from .views import(
+    IdeaListView,
+)
+
 
 urlpatterns = [
-    
+    path('', IdeaListView.as_view(), name = 'blog-home'),
+    # path('about/', , name = 'about-home'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import(
+    ListView,
+    DetailView,
+)
+
+
+
+class IdeaListView(ListView):
+    template_name = 'blog/home.html'
+
