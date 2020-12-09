dae5ecd52c465c6c18e5028bc02c24f103e84adf
Mohammad mahdi <mohammadmahdi@ubuntu.ubuntu-domain>
2020-12-09 23:07:24 +0330

    create signals.py for creating Profile after creating User automaticly & recognizer these in apps.py + little change in Profile model.

--- users/models.py
+++ users/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
 
-
-
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
@@ -11,8 +9,8 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self):
-        super().save()
+    def save(self, *args, **kwargs):
+        super(Profile, self).save(*args, **kwargs)
 
         img = Image.open(self.image.path)
 
@@ -20,3 +18,4 @@ class Profile(models.Model):
             output_size = (300, 300)
             img.thumbnail(output_size)
             img.save(self.image.path)
+
