a805c1a83cd795af895883437145f3ab7cb0af05
Mohammad mahdi <mohammadmahdi@ubuntu.ubuntu-domain>
2020-12-10 01:34:26 +0330

    install CRISPY. handle that in settings.py. add that in register.html.

--- ideablog/settings.py
+++ ideablog/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'blog.apps.BlogConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -123,3 +124,5 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
