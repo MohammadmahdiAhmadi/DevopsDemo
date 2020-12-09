ec30adb02205cbae57770cdb4223f6a602b2fd8e
Mohammad mahdi <mohammadmahdi@ubuntu.ubuntu-domain>
2020-12-10 02:25:29 +0330

    Login-Logout

--- ideablog/urls.py
+++ ideablog/urls.py
@@ -22,13 +22,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from users import views as user_views
-
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('register/', user_views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
 
 if settings.DEBUG:
