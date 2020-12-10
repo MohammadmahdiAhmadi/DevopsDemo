db99fde5948104ab362ee47056370ea10915e98f
Mohammad mahdi <mohammadmahdi@ubuntu.ubuntu-domain>
2020-12-10 20:25:21 +0330

    add get_absolute_url and reverse library in Idea model.

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class Idea(models.Model):
@@ -11,4 +12,8 @@ class Idea(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("idea-detail", kwargs={"pk": self.pk})
+    
     
