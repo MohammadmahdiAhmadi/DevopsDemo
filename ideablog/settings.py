70d9ba94bd4ab3c4a33c970fff3d4e58f196b100
Mohammad mahdi <mohammadmahdi@ubuntu.ubuntu-domain>
2021-02-10 18:48:31 +0330

    DEBUG=False, ALLOWED_HOST= ...

--- ideablog/settings.py
+++ ideablog/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'kdqor=3paqz*ixssz(he4q&6kt%rs27@awva$b!otqo$l1fzg$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ideablog-mmahmadi.fandogh.cloud',]
 
 
 # Application definition
