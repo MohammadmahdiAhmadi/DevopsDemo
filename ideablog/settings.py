b393b752034f22ef5b66f1f840019e068b87b09d
Mohammad mahdi <mohammadmahdi@ubuntu.ubuntu-domain>
2021-02-11 16:46:08 +0330

    Change venv. django==3.1, python==3.7, ... + change in database and migrations + change requirements.txt + add fandog.yml files

--- ideablog/settings.py
+++ ideablog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'kdqor=3paqz*ixssz(he4q&6kt%rs27@awva$b!otqo$l1fzg$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['ideablog-mmahmadi.fandogh.cloud','127.0.0.1']
 
